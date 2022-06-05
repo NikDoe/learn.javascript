cccd178702b9f92703a485e958df3918ba1a5369
NikDoe <nikd0e@yandex.ru>
2022-06-05 18:26:14 +0300

    another some strings methods

--- basics/strings/strings.js
+++ basics/strings/strings.js
@@ -7,3 +7,13 @@ console.log(str);
 // or
 let newStringName = str.toUpperCase(); //new variable that save this changes
 console.log(newStringName);
+
+//indexied
+let pie = "pecan pie";
+console.log(pie[7]);
+
+let park = "Yellowstone";
+const index = park.indexOf('Stone'); //-1, cause no mathes
+console.log(index);
+
+console.log("GARBAGE".slice(2).replace("B", "_"));
