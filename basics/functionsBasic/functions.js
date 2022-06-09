66679f2e0e49432ac30811220ece329b2d2b2c04
NikDoe <nikd0e@yandex.ru>
2022-06-09 18:04:49 +0300

    second challenge of functions is pased

--- basics/functionsBasic/functions.js
+++ basics/functionsBasic/functions.js
@@ -12,3 +12,26 @@ const pass2 = isValidPassword('nikdoe322', 'nikdoe'); //false
 console.log(pass2);
 const pass3 = isValidPassword('1234567', 'nikdoe');
 console.log(pass3);
+
+//challenge 2
+function avg (arr) {
+	let sum = 0;
+	for (let num of arr) {
+		sum += num;
+	}
+	return sum / arr.length;
+}
+
+const avg1 = avg([
+	0,
+	50
+]); //25
+console.log(avg1);
+const avg2 = avg([
+	75,
+	76,
+	80,
+	95,
+	100
+]); // 85.2
+console.log(avg2);
