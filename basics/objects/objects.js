1900f357d17f53b2f1688d9077287c4b44868f43
NikDoe <nikd0e@yandex.ru>
2022-06-10 11:00:48 +0300

    tasks from objects topic are done

--- basics/objects/objects.js
+++ basics/objects/objects.js
@@ -8,6 +8,25 @@ console.log(user);
 delete user.name;
 console.log(user);
 
+//task 2
+let schedule = {};
+
+function isEmpty (obj) {
+	let str = '';
+	for (const key in obj) {
+		str += key;
+	}
+	return !str;
+}
+
+//variant 2
+function isEmptyObject (data) {
+	return Object.keys(data).length === 0;
+}
+
+console.log(isEmpty(schedule));
+console.log(isEmptyObject(schedule));
+
 //task 3
 const newUser = {
 	name : 'John'
@@ -18,3 +37,32 @@ newUser.name = 'Pete';
 
 // Ошибка
 // newUser = 123;
+
+//task 4
+let salaries = {};
+
+function sumSalaries (obj) {
+	let sum = 0;
+	for (const key in obj) {
+		sum += obj[key];
+	}
+	return sum;
+}
+
+console.log(sumSalaries(salaries));
+
+//task 5
+let menu = {
+	width  : 200,
+	height : 300,
+	title  : 'My menu'
+};
+
+function multiplyNumeric (obj) {
+	for (const key in obj) {
+		typeof obj[key] === 'number' && (obj[key] *= 2);
+	}
+}
+
+multiplyNumeric(menu);
+console.log(menu);
