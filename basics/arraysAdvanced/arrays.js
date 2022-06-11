2267be4221672a3aae82d26a05aa6b22bb323afc
NikDoe <nikd0e@yandex.ru>
2022-06-11 14:36:25 +0300

    filter method task is complited

--- basics/arraysAdvanced/arrays.js
+++ basics/arraysAdvanced/arrays.js
@@ -148,3 +148,18 @@ const camelize = (str) => {
 	console.log(newStr);
 	// return;
 };
+
+//task 3
+let arr = [
+	5,
+	3,
+	8,
+	1
+];
+
+const filterRange = (arr, a, b) => {
+	return arr.filter((el) => el >= a && el <= b);
+};
+
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered);
