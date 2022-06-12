76d6aa2f0e9d1a965eeca70cd75b71f1a153a984
NikDoe <nikd0e@yandex.ru>
2022-06-12 14:16:38 +0300

    work with default params

--- basics/functionsAdvansed/functions.js
+++ basics/functionsAdvansed/functions.js
@@ -1,41 +1,46 @@
 //task 1
-function truncate (str, maxlength) {
-	return str.length > maxlength ? str.slice(0, maxlength - 1) + '...' : str;
+function truncate(str, maxlength) {
+    return str.length > maxlength ? str.slice(0, maxlength - 1) + '...' : str;
 }
 
 truncate('Вот, что мне хотелось бы сказать на эту тему:', 20);
 truncate('Всем привет!', 20);
 
 //task 2
-function extractCurrencyValue (str) {
-	let newStr = '';
-	for (let i = 0; i < str.length; i++) {
-		if (!isNaN(str[i])) {
-			newStr += str[i];
-		}
-	}
-	return +newStr;
+function extractCurrencyValue(str) {
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+        if (!isNaN(str[i])) {
+            newStr += str[i];
+        }
+    }
+    return +newStr;
 }
 
 console.log(extractCurrencyValue('$$$$$$$$$$120$') === 120); //true
 
 //task 3
-function sumInput () {
-	const arr = [];
-	let sum = 0;
+function sumInput() {
+    const arr = [];
+    let sum = 0;
 
-	while (true) {
-		const el = prompt('Введите число!');
-		if (el === '' || el === null || isNaN(el)) break;
+    while (true) {
+        const el = prompt('Введите число!');
+        if (el === '' || el === null || isNaN(el)) break;
 
-		arr.push(+el);
-	}
+        arr.push(+el);
+    }
 
-	for (const iterator of arr) {
-		sum += iterator;
-	}
+    for (const iterator of arr) {
+        sum += iterator;
+    }
 
-	return sum;
+    return sum;
 }
 
 console.log(sumInput());
+
+//default params
+const multiply = (x, y = 1) => x * y;
+console.log(multiply(4))
+
