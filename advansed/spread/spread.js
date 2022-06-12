ebde3af6dc733c96ca4ef4b5b6cb750b6749fd90
NikDoe <nikd0e@yandex.ru>
2022-06-12 16:06:20 +0300

    spread operator in object literals

--- advansed/spread/spread.js
+++ advansed/spread/spread.js
@@ -26,3 +26,44 @@ const newArrOfEverything = [
 	'HELLO',
 ];
 console.log(newArrOfEverything);
+
+//spread in objects literals
+const feline = {
+	legs: 4,
+	family: 'Felidae',
+};
+
+const canine = {
+	family: 'Caninae',
+	furry: true,
+	legs: 4,
+};
+
+const dog = {
+	...canine,
+	isPet: true,
+	adorable: true,
+};
+
+console.log(dog); //{family: "Caninae", furry: true, legs: 4, isPet: true, adorable: true}
+
+const catDog = {
+	...canine,
+	...feline,
+};
+
+console.log(catDog); //{family: "Felidae", furry: true, legs: 4}
+
+//Order matters! Legs will be 3 here, because we set it AFTER spreading canine.
+const tripod = {
+	...canine,
+	legs: 3,
+};
+
+//spread object in array
+// const errorObjectSpreadInArray = [...'hello', ...catDog]; //error, objects is not iterable
+const correctObjectSpreadInArray = [...'hello', { ...catDog }];
+
+//spread array in object
+const spreadArrayInObject = { ...['red', 'green', 'yellow'] };
+console.log(spreadArrayInObject); //{0: 'red', 1: 'green', 2: 'yellow'}
