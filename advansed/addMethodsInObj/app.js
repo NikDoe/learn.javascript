00d5c9aec6ee80619abdd252c79c9aa55bcb17b6
NikDoe <nikd0e@yandex.ru>
2022-06-14 10:51:00 +0300

    shorthand of adding methods

--- advansed/addMethodsInObj/app.js
+++ advansed/addMethodsInObj/app.js
@@ -1,13 +1,15 @@
 // Adding methods to an object!
 const math = {
 	numbers: [1, 2, 3, 4, 5],
-	add: function (x, y) {
+	add(x, y) {
 		return x + y;
 	},
-	multiply: function (x, y) {
+	multiply(x, y) {
 		return x * y;
 	},
 };
 
 // To execute multiply:
 math.multiply(5, 9); //45
+const sum = math.add(11, 30); //41
+console.log(sum);
