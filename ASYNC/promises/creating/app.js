af690c96b3203cd02b0881cdf97b3dce6f626fbb
NikDoe <nikd0e@yandex.ru>
2022-06-24 19:01:12 +0300

    refactor returning a promise function

--- ASYNC/promises/creating/app.js
+++ ASYNC/promises/creating/app.js
@@ -1,14 +1,20 @@
-const willGetYouADog = new Promise((resolve, reject) => {
-	const rand = Math.random();
-	if (rand < 0.5) {
-		resolve();
-	} else {
-		reject();
-	}
-});
-willGetYouADog.then(() => {
-	console.log('YAY WE GOT A DOG!!!!');
-});
-willGetYouADog.catch(() => {
-	console.log(':( NO DOG');
-});
+const makeDogPromise = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const rand = Math.random();
+			if (rand < 0.5) {
+				resolve();
+			} else {
+				reject();
+			}
+		}, 3000);
+	});
+};
+
+makeDogPromise()
+	.then(() => {
+		console.log('YAY WE GOT A DOG!!!!');
+	})
+	.catch(() => {
+		console.log(':( NO DOG');
+	});
