e433c94be34d874569704631460dfa3301529ee5
NikDoe <nikd0e@yandex.ru>
2022-06-28 17:32:18 +0300

    refactor using the power of promises

--- ASYNC/requests/fetchAPI/app.js
+++ ASYNC/requests/fetchAPI/app.js
@@ -1,15 +1,20 @@
 fetch('https://swapi.dev/api/planets/')
 	.then(response => {
-		if (!response.ok) {
-			throw new Error(`${response.status}`);
-		} else {
-			response.json().then(data => {
-				const { results } = data;
-				results.forEach(({ name }) => {
-					console.log(name);
-				});
-			});
-		}
+		if (!response.ok) throw new Error(`${response.status}, page not found`);
+
+		return response.json();
+	})
+	.then(data => {
+		const filmURL = data.results[0].films[0];
+		return fetch(filmURL);
+	})
+	.then(response => {
+		if (!response.ok) throw new Error(`${response.status}, page not found`);
+
+		return response.json();
+	})
+	.then(data => {
+		console.log(data.title);
 	})
 	.catch(err => {
 		console.error(err);
