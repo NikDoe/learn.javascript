428fccda6a018eca1a7449fe93c9b78b75e99ee4
NikDoe <nikd0e@yandex.ru>
2022-06-28 18:00:03 +0300

    refactor using DRY & resolve method

--- ASYNC/requests/fetchAPI/app.js
+++ ASYNC/requests/fetchAPI/app.js
@@ -1,21 +1,28 @@
-fetch('https://swapi.dev/api/planets/')
-	.then(response => {
-		if (!response.ok) throw new Error(`${response.status}, page not found`);
+const checkStatusAndParse = response => {
+	if (!response.ok) throw new Error(`${response.status}, page not found`);
 
-		return response.json();
-	})
-	.then(data => {
-		const filmURL = data.results[0].films[0];
-		return fetch(filmURL);
-	})
-	.then(response => {
-		if (!response.ok) throw new Error(`${response.status}, page not found`);
+	return response.json();
+};
 
-		return response.json();
-	})
-	.then(data => {
-		console.log(data.title);
-	})
+const printPlanets = data => {
+	data.results.forEach(({ name }) => {
+		console.log(name);
+	});
+
+	return Promise.resolve(data.next);
+};
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+	return fetch(url);
+};
+
+fetchNextPlanets()
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
 	.catch(err => {
 		console.error(err);
 	});
