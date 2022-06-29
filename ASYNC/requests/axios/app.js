b164ac8f5d23e988abd12add91bba2ab59b477d5
NikDoe <nikd0e@yandex.ru>
2022-06-29 17:34:10 +0300

    using axios instead of fetch API

---
+++ ASYNC/requests/axios/app.js
@@ -0,0 +1,23 @@
+axios
+	.get('https://swapi.dev/api/planets/')
+	.then(res => {
+		//We don't have to parse the JSON!
+		console.log(res.data);
+	})
+	.catch(err => {
+		console.log('IN CATCH CALLBACK!!!');
+		console.log(err);
+	});
+
+axios
+	.get('https://swapi.dev/api/planeafafafts/') //BAD URL!
+	.then(res => {
+		//We don't need to check for a 200 status code, because...
+		//Axios will reject the promise for us, unlike fetch!
+		console.log(res.data);
+	})
+	.catch(err => {
+		//In this example with a not-found URL, this callback will run...
+		console.log('IN CATCH CALLBACK!!!');
+		console.log(err);
+	});
